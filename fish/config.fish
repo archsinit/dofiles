d806d88d4da4a69cecfc40ba8e1577105750b991
archsinit <archsinit@archsinits-MacBook-Pro.local>
2022-11-21 09:33:52 +0900

    update aliases

--- fish/config.fish
+++ fish/config.fish
@@ -17,3 +17,12 @@ alias lta="exa --sort Name --long --tree --all --recurse --level=3"
 # default text editor: neovim
 alias v="nvim"
 alias vi="nvim"
+alias ev="nvim $MYVIMRC"
+alias sv="source $MYVIMRC"
+
+# git aliases
+alias gss="git status"
+alias gpl="git pull"
+alias gad="git add ."
+alias gps="git push origin master"
+alias gcm="git commit -m"
